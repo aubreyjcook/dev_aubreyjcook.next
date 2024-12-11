3bbc0fbb9f3bf4c9567922220cf10ff0d646c53b
aubreyjcook <aubreyjcook@gmail.com>
2024-12-11 07:55:01 -0600

    minor update

--- aubreyjcook.next/app/page.tsx
+++ aubreyjcook.next/app/page.tsx
@@ -1,8 +1,16 @@
+import React from 'react';
+import Head from 'next/head';
 import Image from "next/image";
 
 export default function Home() {
-  return (    
-    <main>
-    </main>
+  return (
+    <div>
+        <Head>
+            <title>Home</title>
+            <meta name="description" content="Home" />
+        </Head>
+        <h1>Content</h1>
+        <p>Content</p>
+    </div>
   );
 };
