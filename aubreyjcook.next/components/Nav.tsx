7fab2c742879875fb000dfae53d81cd3210b4211
aubreyjcook <aubreyjcook@gmail.com>
2025-01-28 22:55:57 -0600

    update nav

--- aubreyjcook.next/components/Nav.tsx
+++ aubreyjcook.next/components/Nav.tsx
@@ -5,16 +5,14 @@ const Nav = () => {
         <nav className="bg-background text-foreground p-4">
             <div className="flex w-full justify-between items-center uppercase">
                 <h1 className="text-xl"><a href="">Aubrey Cook</a></h1>
-                <div className='flex-grow flex justify-end'>
-                    <div className="flex space-x-6 p-1 bg-gray-800 border-gray-500 border-2 text-xl">
+                <div className='flex-grow flex space-x-2 justify-end text-2xl'>
+                    <div className="flex p-1 bg-gray-800 border-gray-500 border-2">
                         <a href="#" className="p-2">Portfolio</a>
                         <a href="#" className="p-2">About</a>
+                        <a href="#" className="p-2">Resume</a>
                         <a href="#" className="p-2">Contact</a>
-                    </div>
-                    <div className="flex ml-6 space-x-6 p-1 bg-gray-800 border-gray-500 border-2 text-xl">
-                        <a href="#">Resume</a>
-                        <a href="#">Blog</a>
-                        <a href="#">Photography</a>
+                        <a href="#" className="p-2">Blog</a>
+                        <a href="#" className="p-2">Photography</a>
                     </div>
                 </div>
             </div>
