6e65a511557ef2422006cbf773060d41075b8735
aubreyjcook <aubreyjcook@gmail.com>
2025-01-30 23:33:09 -0600

    update component:nav;redefining link components

--- aubreyjcook.next/components/Nav.tsx
+++ aubreyjcook.next/components/Nav.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Nav = () => {
     return(
         <nav className="bg-background text-foreground p-4">
             <div className="flex w-full justify-between items-center uppercase">
-                <h1 className="text-xl"><a href="">Aubrey Cook</a></h1>
+                <h1 className="text-xl"><Link href="/">Aubrey Cook</Link></h1>
                 <div className='flex-grow flex space-x-2 justify-end text-2xl'>
                     <div className="flex p-1 bg-gray-800 border-gray-500 border-2">
-                        <a href="#" className="p-2">Portfolio</a>
-                        <a href="#" className="p-2">About</a>
-                        <a href="#" className="p-2">Resume</a>
-                        <a href="#" className="p-2">Contact</a>
-                        <a href="#" className="p-2">Blog</a>
-                        <a href="#" className="p-2">Photography</a>
+                        <Link href="/portfolio" className="p-2">Portfolio</Link>
+                        <Link href="/about" className="p-2">About</Link>
+                        <Link href="/resume" className="p-2">Resume</Link>
+                        <Link href="/contact" className="p-2">Contact</Link>
+                        <Link href="/blog" className="p-2">Blog</Link>
+                        <Link href="/photography" className="p-2">Photography</Link>
                     </div>
                 </div>
             </div>
